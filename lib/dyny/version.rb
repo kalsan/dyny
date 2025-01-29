ca0ee09747af60e055351a2892cda0266729448b
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2025-01-29 17:57:28 +0100

    Switch version detection to text file

--- lib/dyny/version.rb
+++ lib/dyny/version.rb
@@ -2,12 +2,6 @@
 
 module Dyny
   module Version
-    MAJOR = 0
-    MINOR = 0
-    PATCH = 3
-
-    EDGE = false
-
-    LABEL = [MAJOR, MINOR, PATCH, EDGE ? 'edge' : nil].compact.join('.')
+    LABEL = (Pathname.new(__FILE__).dirname.dirname.dirname / 'VERSION').read
   end
 end
